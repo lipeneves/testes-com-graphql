0a113990e8fc82e63e26361d895f485abdd143af
Felipe Neves <88124495+lipeneves@users.noreply.github.com>
2021-08-27 15:53:26 -0300

    Reactjs instalado

--- packages/server/src/main.js
+++ packages/server/src/main.js
@@ -1,6 +1,4 @@
 import { createServer } from "http";
-import { readFile } from "fs";
-import { resolve } from "path";
 import { parse } from "querystring";
 
 const server = createServer((request, response) => {
@@ -18,51 +16,16 @@ const server = createServer((request, response) => {
             response.end();
             break;
 
-
-        case '/sign-in':{
-            const path = resolve(__dirname, "./pages/sign-in.html");
-            readFile(path, (error, file) => {
-                if (error) {
-                    response.writeHead(500, "Can't process HTML file");
-                    response.end();
-                    return;
-                }
-
-                response.writeHead(200);
-                response.write(file);
-                response.end();
-            });
-            break;
-        }
-
         case '/authenticate':
             let data = '';
             request.on('data', (chunk) => {
                 data += chunk;
             });
             request.on('end', ()=>{
-                response.writeHead(301, {
-                    Location: '/home',
-                });
+                const params = parse(data);
                 response.end();
             });
             break;
-
-            case '/home':{
-                const path2 = resolve(__dirname, "./pages/home.html");
-                readFile(path2, (error, file) => {
-                    if (error) {
-                        response.writeHead(500, "Can't process HTML file");
-                        response.end();
-                        return;
-                    }
-    
-                    response.writeHead(200);
-                    response.write(file);
-                    response.end();
-                });
-                break;
-            }
 
         default:
             response.writeHead(404, 'Service not found');
